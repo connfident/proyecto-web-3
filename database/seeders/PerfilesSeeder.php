d3bd0076865ec6241f3160f8d0d292ec229aed00
connfident <soren1735@gmail.com>
2023-07-01 05:17:14 -0400

    seeders update 1.19.2

--- database/seeders/PerfilesSeeder.php
+++ database/seeders/PerfilesSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class PerfilesSeeder extends Seeder
 {
@@ -16,6 +17,10 @@ class PerfilesSeeder extends Seeder
         DB::table('perfiles')->insert([
             ['id'=>1,'nombre'=>'Admin',],
             ['id'=>2,'nombre'=>'Artista',],
+        ]);
+
+        DB::table('cuentas')->insert([
+            ['user' => "Admin", 'password'=>Hash::make('admin'),'nombre'=>'Admin 1','apellido'=>'Admin','perfil_id'=>1],
         ]);
     }
 }
